48c5e38dad2afc5617b8ad3934631e57792412af
Dayana1905 <dayana.cedeno1499@utc.edu.ec>
2024-11-13 17:33:18 -0500

    Update models.py

--- NOTAS/CALIFICACIONES/models.py
+++ NOTAS/CALIFICACIONES/models.py
@@ -1,3 +1,38 @@
+from django.contrib import admin
+
+# Register your models here.
 from django.db import models
 
-# Create your models here.
+class Estudiantes(models.Model):
+    cedula = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=30, blank=False)
+    apellido = models.TextField()
+    edad = models.IntegerField()
+    direccion = models.TextField()
+    telefono = models.CharField(max_length=16)
+
+    def __str__(self):
+        return self.nombre
+
+class Docente(models.Model):
+    cedula = models.CharField(primary_key=True, max_length=10)
+    nombre = models.CharField(max_length=30, blank=False)
+    apellido = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+class Materia(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField(blank=True)
+    docente = models.ForeignKey(Docente, related_name='materias', on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return self.nombre
+
+class Calificaciones(models.Model):
+    estudiante = models.ForeignKey(Estudiantes, on_delete=models.RESTRICT)
+    materia = models.ForeignKey(Materia, on_delete=models.RESTRICT)
+    nota = models.FloatField()
+
+   
